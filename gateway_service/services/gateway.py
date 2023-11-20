3a0debcdaf9a28f4bf047c302ed89eacdae7f887
Михаил Волков <mihailvolkov@192.168.1.9>
2023-11-20 21:29:26 +0300

    Изменено тестирование

--- gateway_service/services/gateway.py
+++ gateway_service/services/gateway.py
@@ -74,7 +74,7 @@ class GatewayService():
         print("reservations_list:",reservations_list)
         reservations = []
 
-        for i in len(reservations_list):
+        for i in range(len(reservations_list)):
             print("i:", i)
             hotel_dict = await self.__get_hotel_by_id(reservations_list[i]["hotel_id"])
             payment_dict = await self.__get_payment_by_uid(reservations_list[i]["payment_uid"])
